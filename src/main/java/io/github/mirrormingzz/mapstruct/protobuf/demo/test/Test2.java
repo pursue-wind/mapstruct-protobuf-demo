679e49450224ebbd862b8b480bf3d102f32581d6
陈静子 <chenjingzi@galasports.com>
2020-12-24 18:45:46 +0800

    Merge branch 'gradle' of D:\Project\my\mapstruct-protobuf-demo with conflicts.

---
+++ src/main/java/io/github/mirrormingzz/mapstruct/protobuf/demo/test/Test2.java
@@ -0,0 +1,13 @@
+package io.github.mirrormingzz.mapstruct.protobuf.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Test2 {
+    String a2;
+}
---
+++ src/main/java/io/github/mirrormingzz/mapstruct/protobuf/demo/test/Test4.java
@@ -0,0 +1,13 @@
+package io.github.mirrormingzz.mapstruct.protobuf.demo.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Test3 {
+    String a4;
+}
